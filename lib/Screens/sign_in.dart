4725b73ba585c3a4bdaa9b046c5c134077f9c378
Aman Khan <8742909496aman@gmail.com>
2024-05-08 20:48:41 +0530

    sign in and sign up screen created

--- lib/Screens/sign_in.dart
+++ lib/Screens/sign_in.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatelessWidget {
-  const SignInScreen({super.key});
-  static String routeName = '/sign_in';
+  const SignInScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     color: Colors.white,
+          //     borderRadius: BorderRadius.circular(20.0),
+          //   ),
+          //   child: Image.asset(
+          //     'assets/images/splash_1024.png',
+          //     height: 65,
+          //     width: 63,
+          //   ),
+          // ),
+          Text(
+            'Hello',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 8,
+              ),
+              Text(
+                'Welcome to Weight Tracker',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          // AuthForm(
+          //   model: model,
+          // ),
+          Container(
+            color: Colors.white,
+            child: TextFormField(
+              key: ValueKey('username'),
+              validator: (value) {
+                if (value == null || value.length <= 3) {
+                  return 'Please enter a valid username';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                icon: Icon(Icons.person),
+                labelText: 'UserName',
+              ),
+              onSaved: (value) {
+                // widget.model.userName = value as String;
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: (){
+              // Navigator.pushNamed(context, '/signup');
+            },
+            child: Text('Sign In'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.green, // background
+              backgroundColor: Colors.white, // foreground
+            ),
+          ),
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Text(
+          //     'By tapping SignUp I agree to Terms of service.Privacy Policy and User Agreement. ',
+          //     textAlign: TextAlign.center,
+          //     style: TextStyle(
+          //       color: Colors.white,
+          //       fontWeight: FontWeight.w500,
+          //       fontSize: 13,
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+    );
   }
 }
